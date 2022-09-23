bef192e4a2eee2b9674b056c2875a8cad049798f
Vinhuanc <102390736+Vinhuanc@users.noreply.github.com>
2022-09-22 20:25:04 -0500

    Update Assig02-Prog01.cpp

--- Assignment02/Assig02-Prog01.cpp
+++ Assignment02/Assig02-Prog01.cpp
@@ -1,4 +1,17 @@
 //Code for Assignment02 Program#1
+// Program #1
+// Write a program that computes the tax and tip on a restaurant bill for a meal charge. The tax should be 6.75 percent of the meal cost. The tip should be 20 percent of the total after adding the tax. Display the meal cost, tax amount, tip amount, and total bill on the screen.
+// • Use a const (constant) for the tax and tip rates (0.0675 and 0.2 respectively)
+// • Ask the user for the cost of the meal
+// Sample Output:
+// Meal: $85.45
+// Tax: $5.76788
+// Tip: $18.2436
+// Total: $109.461
+// Press any key to continue . . .
+// Test Cases to try and think about:
+// • How does your program respond when the values entered are negative?
+// • How does your program respond when the values entered are smaller than a penny?
 #include <iostream>
 #include <string>
 using namespace std;
